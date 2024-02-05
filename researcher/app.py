24603f247b0fefdf621e697952152b90733a0bd4
kforcodeai <kaushalcse07@gmail.com>
2024-02-05 15:51:19 +0530

    plotting plotly

--- researcher/app.py
+++ researcher/app.py
@@ -6,6 +6,7 @@ import pickle
 import streamlit.components.v1 as components
 import plotly.graph_objects as go
 
+
 # Streamlit app
 def main():
     st.title("Paper Search and Plot App")
@@ -17,28 +18,29 @@ def main():
 
     # Slider for number of results
     num_results = st.slider(
-        "Select number of results to return", min_value=2, max_value=20, value=5
+        "Select number of results to return", min_value=2, max_value=100, value=10
     )
 
     # Button to trigger the search and plot
     if st.button("Search and Plot"):
-        
         G = create_connection_plot(keyword, num_results)
-        # pickle.dump(G, open('graph.pkl', 'wb'))
-        # G = pickle.load(open('graph.pkl', 'rb'))
 
-        pos = nx.spring_layout(G, weight='weight')  # positions for all nodes based on edge weight
-        node_size_weights = [sum([d['weight'] for (_, v, d) in G.edges(data=True) if v == n]) for n in G.nodes()]
-        node_sizes =[x*100/ sum(node_size_weights) for x in node_size_weights]
+        pos = nx.spring_layout(
+            G, weight="weight"
+        )  # positions for all nodes based on edge weight
+        node_size_weights = [
+            sum([d["weight"] for (_, v, d) in G.edges(data=True) if v == n])
+            for n in G.nodes()
+        ]
+        node_sizes = [x * 500 / sum(node_size_weights) for x in node_size_weights]
 
-        colors = ['blue', 'red', 'green', 'yellow', 'orange', 'purple']
+        colors = ["blue", "red", "green", "yellow", "orange", "purple"]
 
         node_colors = []
         for i in range(len(G.nodes())):
             idx = node_size_weights[i]
-            idx = idx%len(colors)
+            idx = idx % len(colors)
             node_colors.append(colors[idx])
-
 
         edge_x = []
         edge_y = []
@@ -49,10 +51,12 @@ def main():
             edge_y.extend([y0, y1, None])
 
         edge_trace = go.Scatter(
-            x=edge_x, y=edge_y,
-            line=dict(width=0.5, color='#888'),
-            hoverinfo='none',
-            mode='lines')
+            x=edge_x,
+            y=edge_y,
+            line=dict(width=0.5, color="#888"),
+            hoverinfo="none",
+            mode="lines",
+        )
 
         node_x = []
         node_y = []
@@ -62,69 +66,62 @@ def main():
             x, y = pos[node]
             node_x.append(x)
             node_y.append(y)
-            node_text.append(G.nodes.get(node)['arxivlink'])
+            node_text.append(G.nodes.get(node)["title"])
 
         node_trace = go.Scatter(
-            x=node_x, y=node_y, 
-            mode='markers',
-            hoverinfo='text',
+            x=node_x,
+            y=node_y,
+            mode="markers",
+            hoverinfo="text",
             marker=dict(
                 size=node_sizes,
                 showscale=True,
-                colorscale='YlGnBu',
+                colorscale="YlGnBu",
                 reversescale=True,
                 color=node_colors,
                 # size=10,
                 colorbar=dict(
                     thickness=15,
-                    title='Node Connections',
-                    xanchor='left',
-                    titleside='right'
+                    title="Node Connections",
+                    xanchor="left",
+                    titleside="right",
                 ),
-                line_width=2))
+                line_width=2,
+            ),
+        )
 
         node_trace.text = node_text
 
+        def node_click_callback(trace, points, state):
+            print("here")
+            print("Node clicked:", points.point_inds[0])
+            st.write(trace, state)
 
-        # Define the on_click behavior
-        # node_trace.on_click(lambda x, y: print(f"Node ID: {x}"))
+        fig = go.FigureWidget(
+            data=[edge_trace, node_trace],
+            layout=go.Layout(
+                title="Network Graph with Plotly",
+                titlefont_size=16,
+                showlegend=False,
+                hovermode="closest",
+                margin=dict(b=20, l=5, r=5, t=40),
+                annotations=[
+                    dict(
+                        text="Python code: <a href='https://plotly.com/python/'>Plotly</a>",
+                        showarrow=False,
+                        xref="paper",
+                        yref="paper",
+                        x=0.005,
+                        y=-0.002,
+                    )
+                ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+            ),
+        )
 
-        # # Define the callback function
-        # def display_params(trace, points, selector):
-        #     print('clicked')
-        #     print(trace, points, selector)
-        #     point_id = points.point_inds[0]
-        #     params = trace.customdata[point_id]
-        #     print(params)  # Replace with your desired action to display the parameters
-
-        # # Add the on_click event to the node_trace
-        # node_trace.on_click(display_params)
-
-        fig = go.Figure(data=[edge_trace, node_trace],
-                        layout=go.Layout(
-                            title='Network Graph with Plotly',
-                            titlefont_size=16,
-                            showlegend=False,
-                            hovermode='closest',
-                            margin=dict(b=20, l=5, r=5, t=40),
-                            annotations=[dict(
-                                text="Python code: <a href='https://plotly.com/python/'>Plotly</a>",
-                                showarrow=False,
-                                xref="paper", yref="paper",
-                                x=0.005, y=-0.002)],
-                            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
-                        ))
-        fig.add_trace(node_trace)
-
-        # Plot on Streamlit
-        clicked_node = st.plotly_chart(fig, use_container_width=True)
-        # nodeskey = list(G.nodes.keys())
-        # while clicked_node:
-        #     # st.write(G.nodes.get(clicked_node)['title'])
-        #     idx = clicked_node.__dict__['_provided_cursor'].__dict__['_index']
-        #     _id = nodeskey[idx]
-        #     st.write(G.nodes.get(_id))
+        st.plotly_chart(fig)
+        fig.data[1].on_click(node_click_callback)
 
 
 if __name__ == "__main__":
